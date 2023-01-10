189211c30c0075227b04d4f7ea8959937e45bbf0
Shayan Fahmi <shayan.fahmi@gmail.com>
2023-01-10 21:48:56 +0530

    Models in MVC

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
 {
 	public class HomeController : Controller
 	{
-		public JsonResult Index() {
-			return Json(new { id=1, name="test"});
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+		{
+			_employeeRepository = employeeRepository;
+		}
+
+		public string Index() {
+			return _employeeRepository.GetEmployee(1).Name ;
 		}
 	}
 }
---
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "test@test.com"},
+                new Employee() { Id = 2, Name = "John", Department = "IT", Email = "john@test.com"},
+                new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@test.com"}
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
+
